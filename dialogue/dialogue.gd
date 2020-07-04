7d406f3cc8faa788fc6d2e6172502bc4a319e616
NoahJacobsen <noahedjacobsen@gmail.com>
2020-07-04 16:31:53 -0700

    Adding npc

---
+++ dialogue/dialogue.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var content
+var page = 0
+const DEFAULT_DIALOGUE = ["THIS NPC HAS NO DIALOGUE"]
+
+
+func _ready():
+	visible = false
+	if content.empty():
+		content = DEFAULT_DIALOGUE
+
+func _make_active():
+	visible = true
+	page = 0
+	$Label.text = content[page]
+
+func _make_inactive():
+	visible = false
+
+func _flip_page():
+	if page < content.size() - 1:
+		page += 1
+		$Label.text = content[page]
+	else:
+		_make_inactive()
---
+++ npc/npc.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+export(Array, String) var dialogue
+export(Texture) var npc_texture
+
+var showing_text = false
+
+func _ready():
+	$Sprite.texture = npc_texture
+	$Dialogue.content = dialogue
+
+func interact_enter():
+	if !showing_text:
+		$Dialogue._make_active()
+		showing_text = true
+
+func interact():
+	if showing_text:
+		$Dialogue._flip_page()
+	
+func interact_exit():
+	if showing_text:
+		$Dialogue._make_inactive()
+		showing_text = false
