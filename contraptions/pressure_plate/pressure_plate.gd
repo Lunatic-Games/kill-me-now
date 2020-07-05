12f3a6e153989072f13ebaf638bec38649658a17
Joe <joe.zlonicky@gmail.com>
2020-07-04 22:01:18 -0700

    Pressure plate animation

--- contraptions/pressure_plate/pressure_plate.gd
+++ contraptions/pressure_plate/pressure_plate.gd
@@ -7,8 +7,10 @@ signal toggled
 func _on_Area2D_area_entered(area):
 	if area.name == "Feet":
 		emit_signal("toggled", true)
+		$AnimationPlayer.play("pressed")
 
 
 func _on_Area2D_area_exited(area):
 	if area.name == "Feet":
 		emit_signal("toggled", false)
+		$AnimationPlayer.play("released")
