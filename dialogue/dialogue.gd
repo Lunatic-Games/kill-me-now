103440ded18522b4327494045001de25c16560ed
NoahJacobsen <noahedjacobsen@gmail.com>
2020-07-04 17:32:13 -0700

    Improved dialogue printing

--- dialogue/dialogue.gd
+++ dialogue/dialogue.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var content
 var page = 0
+var feed = ""
+var text = ""
 const DEFAULT_DIALOGUE = ["THIS NPC HAS NO DIALOGUE"]
 
 
@@ -13,14 +15,35 @@ func _ready():
 func _make_active():
 	visible = true
 	page = 0
-	$Label.text = content[page]
+	feed = content[page]
+	$Timer.start()
 
 func _make_inactive():
 	visible = false
+	text = ""
+	feed = ""
+	$Label.text = ""
+	$Timer.stop()
 
 func _flip_page():
 	if page < content.size() - 1:
 		page += 1
-		$Label.text = content[page]
+		text += feed + "\n"
+		feed = content[page]
 	else:
-		_make_inactive()
+		if feed.empty():
+			_make_inactive()
+		else:
+			text += feed
+			feed = ""
+			$Label.text = text
+
+func _on_timeout():
+	if !feed.empty():
+		text += feed[0]
+		feed.erase(0,1)
+		if !feed.empty():
+			if feed[0] == " ":
+				text += feed[0]
+				feed.erase(0,1)
+	$Label.text = text
